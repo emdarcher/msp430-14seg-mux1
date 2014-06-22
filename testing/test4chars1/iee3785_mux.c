5b5de1011fd74ce4e7ff58c4a8cf7ce3a775eaf2
emdarcher <emd519@yahoo.com>
2014-06-22 00:21:44 -0700

    finished first set of chars in ascii with segments and some vars

--- testing/test4chars1/iee3785_mux.c
+++ testing/test4chars1/iee3785_mux.c
@@ -5,3 +5,21 @@
 #include <stdint.h>
 #include "iee3785_mux.h"
 
+
+//must edit this from wiring
+/*
+void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t val)
+{
+        uint8_t i;
+
+        for (i = 0; i < 8; i++)  {
+                if (bitOrder == LSBFIRST)
+                        digitalWrite(dataPin, !!(val & (1 << i)));
+                else    
+                        digitalWrite(dataPin, !!(val & (1 << (7 - i))));
+                        
+                digitalWrite(clockPin, HIGH);
+                digitalWrite(clockPin, LOW);            
+        }
+}
+*/
